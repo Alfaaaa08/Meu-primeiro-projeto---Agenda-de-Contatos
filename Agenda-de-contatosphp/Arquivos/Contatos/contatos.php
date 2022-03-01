3ea0c81f0a28c893ebfd8fb6b03f8ab74b574775
Alfaaaa08 <100720882+Alfaaaa08@users.noreply.github.com>
2022-03-01 17:28:34 -0300

    Update contatos.php

--- Agenda-de-contatosphp/Arquivos/Contatos/contatos.php
+++ Agenda-de-contatosphp/Arquivos/Contatos/contatos.php
@@ -4,6 +4,12 @@
 <div>
     <a href="index.php?menuop=cadastro_de_Contatos"> Novo Contato</a>
 </div>
+<div>
+    <form action="index.php?menuop=Contatos" method="POST">
+        <input type="text" name="txt_pesquisa">
+        <input type="submit" value="Pesquisar">
+    </form>
+</div>
 <table border="2">
     <thead>
         <tr>
@@ -19,7 +25,12 @@
     </thead>
     <tbody>
         <?php
-        $sql = "SELECT * FROM contatos";
+        $txt_pesquisa =(isset($_POST['txt_pesquisa']))?$_POST['txt_pesquisa']:"";
+        $sql = "SELECT * FROM contatos 
+        WHERE 
+        fk_Pessoa='{$txt_pesquisa}' or
+        Nome_Contato LIKE '%{$txt_pesquisa}%'
+        ";
         $rs = mysqli_query($conexao, $sql) or die("Erro de execução"
             . mysqli_error($conexao));
         while ($dados = mysqli_fetch_assoc($rs)) {
