500568afb14fa9efc614d3c43e5e777ca0d1f592
Alfaaaa08 <100720882+Alfaaaa08@users.noreply.github.com>
2022-03-01 22:17:46 -0300

    Delete DB directory

--- DB/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-//Constantes de configuração DB;
-define("SERVER", "localhost");
-define("USER", "root");
-define("PASSWORD", "");
-define("BANK", "db_agenda_contatos");
--- DB/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include("config.php");
-
-$conexao = mysqli_connect(SERVER,USER, PASSWORD, BANK) or die("Erro na conexão"
- .mysqli_connect_error());
- 
